fc7c1a7498ae2f81f62ceeb7617893894f5f61cc
Akinori Wada <ak_wada@outlook.com>
2024-10-02 21:07:05 -0500

    The cells can now absorb each other

--- src/cellabsorption/CellSimulation.java
+++ src/cellabsorption/CellSimulation.java
@@ -30,11 +30,22 @@ public class CellSimulation {
             for(Cell i: cells){
             Point canvasCenter = new Point(canvas.getWidth() / 2.0, canvas.getHeight() / 2.0);
             i.moveAround(canvasCenter);
-            i.grow(0.02);
             }
 
             canvas.draw();
             canvas.pause(10);
+            handleCellInteraction();
+        }
+    }
+
+    private void handleCellInteraction() {
+        for (int i = 0; i < cells.size(); i++) {
+            Cell cell0 = cells.get(i);
+            for (int j = i + 1; j < cells.size(); j++) {
+                Cell cell1 = cells.get(j);
+                cell0.interactWith(cell1);
+                // TODO: insert call here to make cell0 interact with cell1
+            }
         }
     }
 
